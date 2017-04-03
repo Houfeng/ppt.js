5e516d43d01e5b327b3560d58a84895e9bd243d2
zhanfeng.hzf <admin@xhou.net>
2017-04-03 16:05:44 +0800

    1.0.5

--- src/components/slide/index.js
+++ src/components/slide/index.js
@@ -5,9 +5,10 @@ module.exports = new mokit.Component({
   template: require('./index.html'),
   data() {
     return {
-      color: '#000',
+      bgcolor: '#000',
       style: '',
-      content: ''
+      content: '',
+      align: 'left'
     };
   },
   onCreate() {
@@ -15,9 +16,10 @@ module.exports = new mokit.Component({
     index = parseInt(index) - 1;
     let slide = this.$root.slides[index];
     this.content = slide.innerHTML;
-    this.color = slide.getAttribute('bgcolor') || 'rgb(' + random(20, 150) + ',' +
+    this.bgcolor = slide.getAttribute('bgcolor') || 'rgb(' + random(20, 150) + ',' +
       random(20, 180) + ',' +
       random(20, 180) + ')';
+    this.align = slide.getAttribute('align') || 'left';
     this.style = slide.getAttribute('style') || '';
   }
 });
